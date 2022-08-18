087bff428af417e5ae78b2d80b2891ab216212dd
Arif H.A <48403225+arifayabakan@users.noreply.github.com>
2022-08-18 22:42:01 +0300

    Update Form3.cs

--- Form3.cs
+++ Form3.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form4 : Form
+    public partial class Form3 : Form
     {
 
         private void button2_Click(object sender, EventArgs e)
